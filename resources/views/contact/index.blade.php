322817299a65ff6ba1bebb8f77486ee0493a907f
motoyu5623 <motoyu5623@gmail.com>
2020-10-06 17:33:57 +0900

    [add]ContactFormコントローラーの作成
    CRUD全て作成済み

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form action="{{ route('contact.create') }}" method="get">
+                      <button type="submit" class="btn btn-primary">新規登録</button>
+                    </form>
+
+                    <table class="table">
+                      <thead>
+                        <th scope="col">ID</th>
+                        <th scope="col">氏名</th>
+                        <th scope="col">件名</th>
+                        <th scope="col">登録日時</th>
+                        <th scope="col">詳細</th>
+                      </thead>
+                      <tbody>
+                        @foreach($contacts as $contact)
+                        <tr>
+                          <th>{{ $contact->id }}</th>
+                          <td>{{ $contact->your_name }}</td>
+                          <td>{{ $contact->title }}</td>
+                          <td>{{ $contact->created_at }}</td>
+                          <td><a href="{{route('contact.show', ['id' => $contact->id])}}">詳細を見る</a></td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,19 @@ Route::get('/', function () {
 });
 
 Route::get('tests/test','TestController@index');
+
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
+    Route::get('index','ContactFormController@index')->name('contact.index');
+    Route::get('create','ContactFormController@create')->name('contact.create');
+    Route::post('store','ContactFormController@store')->name('contact.store');
+    Route::get('show/{id}','ContactFormController@show')->name('contact.show');
+    Route::get('edit/{id}','ContactFormController@edit')->name('contact.edit');
+    Route::get('update/{id}','ContactFormController@update')->name('contact.update');
+    Route::get('destroy/{id}','ContactFormController@destroy')->name('contact.destroy');
+});
+
 Auth::routes();
+
+// Route::resource('contacts', 'ContactFormController');
 
 Route::get('/home', 'HomeController@index')->name('home');
